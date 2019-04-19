8a9b0a4f6be4e982f95013ea4d349f321fddd708
Adrián García <adriangarciagon@hotmail.com>
2019-04-20 00:30:25 +0200

    Creado primeras vistas

---
+++ app/src/main/java/com/kidev/adrian/scooterapp/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kidev.adrian.scooterapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kidev.adrian.scooterapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
