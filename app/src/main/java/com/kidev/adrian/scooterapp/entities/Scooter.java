33c7c977d201d93c8538c86ac4e946c4f52790cd
Adrián García González <agarcia.gonzalez@atsistemas.com>
2019-05-21 00:12:46 +0200

    Ahora las Scooters se muestran correctamente en el mapa.

---
+++ app/src/main/java/com/kidev/adrian/scooterapp/entities/Scooter.java
@@ -0,0 +1,77 @@
+package com.kidev.adrian.scooterapp.entities;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.sql.Date;
+
+public class Scooter {
+
+    private Integer id;
+    private LatLng posicion;
+    private Date fechaCompra;
+    private String noSerie;
+    private String matricula;
+    private Integer codigo;
+
+
+    public Scooter() {
+
+    }
+
+    public Scooter(Integer id, LatLng posicion, Date fechaCompra, String noSerie, String matricula, Integer codigo) {
+        this.id = id;
+        this.posicion = posicion;
+        this.fechaCompra = fechaCompra;
+        this.noSerie = noSerie;
+        this.matricula = matricula;
+        this.codigo = codigo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LatLng getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(LatLng posicion) {
+        this.posicion = posicion;
+    }
+
+    public Date getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public String getNoSerie() {
+        return noSerie;
+    }
+
+    public void setNoSerie(String noSerie) {
+        this.noSerie = noSerie;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+}
--- app/src/main/java/com/kidev/adrian/scooterapp/fragments/MapFragment.java
+++ app/src/main/java/com/kidev/adrian/scooterapp/fragments/MapFragment.java
@@ -1,7 +1,9 @@
 package com.kidev.adrian.scooterapp.fragments;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -18,8 +20,10 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.kidev.adrian.scooterapp.R;
+import com.kidev.adrian.scooterapp.entities.Scooter;
 import com.kidev.adrian.scooterapp.util.CallbackRespuesta;
 import com.kidev.adrian.scooterapp.util.ConectorTCP;
 import com.kidev.adrian.scooterapp.util.Util;
@@ -27,7 +31,7 @@ import com.kidev.adrian.scooterapp.util.Util;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private MapView mMapView;
     private GoogleMap mMap;
@@ -39,15 +43,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-
-
+        View include = (View) view.findViewById(R.id.include);
+        surface = (LinearLayout) view.findViewById(R.id.include);
         mMapView = (MapView) view.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
+
+        surface.setVisibility(View.GONE);
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
@@ -71,45 +76,112 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
-        googleMap.setMyLocationEnabled(true);
+        mMap.setOnMarkerClickListener(this);
 
-        Location myPos = googleMap.getMyLocation();
+        if(!mMap.isMyLocationEnabled())
+            mMap.setMyLocationEnabled(true);
 
-        Map<String,String> parametros = new HashMap<String,String>();
-        parametros.put("lat", "1");
-        parametros.put("lon", "1");
+        /*LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Location myLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        ConectorTCP.getInstance().realizarConexion("getScooters", parametros, new CallbackRespuesta() {
-            @Override
-            public void success(Map<String, String> contenido) {
-                int length = Integer.parseInt(contenido.get("length"));
-                Log.i("Conexión exitosa", "Se han recuperado "+length+ " scooters");
+        if (myLocation == null) {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            String provider = lm.getBestProvider(criteria, true);
+            myLocation = lm.getLastKnownLocation(provider);
+        }*/
 
-                for (int i = 0; i < length; i++) {
-                    float lat = Float.parseFloat(contenido.get("posicionLat["+i+"]"));
-                    float lon = Float.parseFloat(contenido.get("posicionLon["+i+"]"));
+        //if(myLocation!=null) {
+            //final LatLng userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
 
-                    LatLng coordenadas = new LatLng(lat, lon);
 
-                    mMap.addMarker(new MarkerOptions()
-                                    .position(coordenadas)
-                                    .title(contenido.get("matricula["+i+"]"))
-                    );
+            double latitude = 1;
+            double longitude = 1;
 
-                    if (i==0)
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordenadas));
+            Map<String, String> parametros = new HashMap<String, String>();
+            parametros.put("lat", latitude + "");
+            parametros.put("lon", longitude + "");
+
+            ConectorTCP.getInstance().realizarConexion("getScooters", parametros, new CallbackRespuesta() {
+                @Override
+                public void success(Map<String, String> contenido) {
+                    int length = Integer.parseInt(contenido.get("length"));
+                    Log.i("Conexión exitosa", "Se han recuperado " + length + " scooters");
+
+                    LatLng pos = new LatLng(0,0);
+
+                    for (int i = 0; i < length; i++) {
+                        int id = Integer.parseInt(contenido.get("id[" + i + "]"));
+                        int codigo = Integer.parseInt(contenido.get("codigo[" + i + "]"));
+                        float lat = Float.parseFloat(contenido.get("posicionLat[" + i + "]"));
+                        float lon = Float.parseFloat(contenido.get("posicionLon[" + i + "]"));
+
+                        Scooter scooter = new Scooter();
+                        scooter.setId(id);
+                        scooter.setCodigo(codigo);
+                        scooter.setPosicion(new LatLng(lat, lon));
+
+                        mMap.addMarker(new MarkerOptions()
+                                .position(scooter.getPosicion())
+                        ).setTag(scooter);
+
+                        if (i==0)
+                            pos = new LatLng(lat, lon);
+                    }
+
+                    mMap.moveCamera((CameraUpdateFactory.newLatLng(pos)));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos, 14), 1500, null);
+
                 }
-            }
 
-            @Override
-            public void error(Map<String, String> contenido, Util.CODIGO codigoError) {
-                Log.e("Error de conexión", "No se han cargado las scooters " + codigoError.toString());
-            }
-        });
+                @Override
+                public void error(Map<String, String> contenido, Util.CODIGO codigoError) {
+                    Log.e("Error de conexión", "No se han cargado las scooters " + codigoError.toString());
+                }
+            });
+        //} else {
+        //    Log.e("MAP ERROR", "No se ha podido cargar la posición de las Scooters");
+        //}
+    }
 
+    @Override
+    public void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 14), 1500, null);
+        Scooter scooter = (Scooter) marker.getTag();
+        if (surface!=null) {
+            surface.setVisibility(View.VISIBLE);
+        } else {
+            Log.e("ERROR map", "El surface no es visible");
+        }
+
+        return true;
     }
 }
